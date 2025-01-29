c8dc324775b40fb7dbe8de774036ccd90a4a3470
Capt Nork <138743746+norkart1@users.noreply.github.com>
2025-01-29 14:47:57 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('./config/db');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/users', userRoutes);
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
